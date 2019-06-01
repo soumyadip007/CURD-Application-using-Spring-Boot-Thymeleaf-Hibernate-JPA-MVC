382541b5e85073f20d814c47b5e5bd96917e07d8
soumyadip007 <soumyadip.note@gmail.com>
2019-06-01 18:27:08 +0530

    Delete method added (bootstrap bug exists)

--- thymeleaf/src/main/java/com/spring/boot/thymeleaf/controller/EmployeeController.java
+++ thymeleaf/src/main/java/com/spring/boot/thymeleaf/controller/EmployeeController.java
@@ -77,6 +77,18 @@ public class EmployeeController {
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
 	}
+	
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("id") int theId) {
+		
+		// delete the employee
+		employeeService.deleteById(theId);
+		
+		// redirect to /employees/list
+		return "redirect:/employees/list";
+		
+	}
 }
 
 
