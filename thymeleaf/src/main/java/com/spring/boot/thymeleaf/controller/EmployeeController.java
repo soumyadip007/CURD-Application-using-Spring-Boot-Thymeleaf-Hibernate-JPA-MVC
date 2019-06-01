79aff948fff2b99d86ad8cd6c57041edbef9620a
soumyadip007 <soumyadip.note@gmail.com>
2019-06-01 18:25:59 +0530

    Update method added

--- thymeleaf/src/main/java/com/spring/boot/thymeleaf/controller/EmployeeController.java
+++ thymeleaf/src/main/java/com/spring/boot/thymeleaf/controller/EmployeeController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.boot.thymeleaf.controller.entity.Employee;
 import com.spring.boot.thymeleaf.service.EmployeeService;
@@ -47,7 +50,32 @@ public class EmployeeController {
 		
 		theModel.addAttribute("employee", theEmployee);
 		
-		return "employees/employee-form";
+		return "form";
+	}
+	
+
+	@GetMapping("/update")
+	public String showFormForUpdate(@RequestParam("id") int theId,
+									Model theModel) {
+		
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+		
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+		
+		// send over to our form
+		return "form";			
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
 
